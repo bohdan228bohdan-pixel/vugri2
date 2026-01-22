6a2cb7c8b11fbc5cb09fb2b905359be1b43556bb
bohdan228bohdan-pixel <bohdan228bohdan@gmail.com>
2026-01-22 11:37:31 +0200

    Update ALLOWED_HOSTS for production and local use

--- vugri/settings.py
+++ vugri/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-dev-secret-key')
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["vugri2.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
